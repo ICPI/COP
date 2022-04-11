afa1aa387fb444955aa9c136a525a21b49615b03
cmkalman <98411193+cmkalman@users.noreply.github.com>
2022-04-11 10:51:09 -0400

    Fixed errors and added more comments/instructions

--- SDSMaps.R
+++ SDSMaps.R
@@ -28,9 +28,11 @@ head(data)
 CoteDivore<-merge(shp,data,by="orgunit_na")
 
 ##mapping in Leaflet----
+
 #install mapping libraries
 install.packages("leaflet")
 library(leaflet)
+
 install.packages("BAMMtools")
 library(BAMMtools)
 
@@ -48,6 +50,10 @@ leaflet(CoteDivore)%>%
                 addLegend('bottomleft', pal = col_pal,values = CoteDivore@data$Value, title = 'TX CURR (FY22)',opacity=1)
 
 ##Mapping in ggplot----
+
+install.packages("ggmap")
+library("ggmap")
+
 #convert shapefile to dataframe (necessary for ggplot2); 
 CoteDivore_shp<-fortify(CoteDivore,region="uid")
 
@@ -65,31 +71,37 @@ display.brewer.all() #this allows you to see your color options
 Breaks<-getJenksBreaks(CoteDivore@data$Value,6)
 Breaks<-Breaks[-1] #this removes the lowest value to fix an issue with how the legend was showing up
 
+#if you have your own google API, replace the key here
+register_google(key="AIzaSyAiHEE5EXfLCFjvamca4DksrNGz9qrg71Q")
+basemap<-get_map(location="Ivory Coast",zoom=7,maptype="roadmap",source="google",color="bw")
+
 #plot the map
 m<-ggmap(basemap)+
   geom_polygon(CoteDivore_df,mapping=aes(x=long,y=lat,group=group,fill=Value),color="#aeaeae",lwd=0.25)+
   scale_fill_fermenter("TX_CURR",type="seq",
-                     palette="Oranges",
+                     palette="Oranges", #repalce this with color schemes from the rColorBrewer package. Available options were plotted previously in the script.
                      direction=1,
                      aesthetics="fill",
                      na.value="#eaeaea",
                      breaks=rev(Breaks),
                      guide=guide_legend(),
-                     labels=c(">45,538",
+                     labels=c(">45,538", #change these based on the Breaks variable calculated above
                               "15,298-45,538",
                               "8,440-15,398",
                               "4,882-8,440",
                               "\u2264 4,882"))+
   coord_equal()+
   theme_void()+
+  labs(title="TX CURR",caption="Source: PEPFAR Panorama")+
   theme(legend.position = "bottom",
         legend.direction="vertical",
         legend.justification="left",
         legend.title = element_text("Currently on Treatment"),
-        plot.margin = margin(0,1,0,1, "cm"))
+        plot.margin = margin(0,0.15,0,0.15, "cm"),
+        plot.caption = element_text(hjust = 0, face= "italic"), #Default is hjust=1
+        plot.caption.position =  "plot",
+        plot.title=element_text(hjust=0))
 m
-
-plot_grid(m,m,m,nrow=1,ncol=3)
 
 #save the map to file
 ggsave(m,"<your file path here>/<file name>.png")
